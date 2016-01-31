80375123f5fc132e684aa49a967e4f60b7be8a96
Ming <mingyli34@gmail.com>
2016-01-30 22:16:46 -0800

    button change image

--- app/src/main/java/mingyli/com/cuts/MainActivity.java
+++ app/src/main/java/mingyli/com/cuts/MainActivity.java
@@ -4,16 +4,36 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class MainActivity extends ActionBarActivity {
+    ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addListenerOnButton();
     }
 
+    public void addListenerOnButton(){
+        image = (ImageView) findViewById(R.id.imageView);
+
+        Button button = (Button) findViewById(R.id.btnChangeImage);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                image.setImageResource(R.drawable.hair);
+            }
+
+        });
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
