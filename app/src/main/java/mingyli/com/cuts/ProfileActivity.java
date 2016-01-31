90490d22cf8d05c3cf876a412254c7bea531f608
Ming <mingyli34@gmail.com>
2016-01-31 03:26:19 -0800

    profile activity contains image

--- app/src/main/java/mingyli/com/cuts/ProfileActivity.java
+++ app/src/main/java/mingyli/com/cuts/ProfileActivity.java
@@ -1,18 +1,11 @@
 package mingyli.com.cuts;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-/**
- * Created by Ming on 1/31/2016.
- */
+
 public class ProfileActivity extends Activity {
 
 
@@ -28,7 +21,6 @@ public class ProfileActivity extends Activity {
 
         ImageView iv = (ImageView) findViewById(R.id.imageView2);
         iv.setImageResource(ImageAdapter.images[pos]);
-//        iv.setImageResource(Resources.getSystem().getIdentifier("haircut" + pos, "drawable", "com.mingyli"));
         iv.setScaleType(ImageView.ScaleType.CENTER_CROP);
     }
 }
