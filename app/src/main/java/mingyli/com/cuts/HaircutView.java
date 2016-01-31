24e3cf8de372d78038d6c138940b3a8c1f377263
wgan <wgan856@gmail.com>
2016-01-30 23:47:10 -0800

    stuff

---
+++ app/src/main/java/mingyli/com/cuts/HaircutView.java
@@ -0,0 +1,13 @@
+package mingyli.com.cuts;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HaircutView extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_haircut_view);
+    }
+}
